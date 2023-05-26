89a394fd0d72590d064ce08f1284c829b12c2c34
willuhmjs <willuhmjs@gmail.com>
2023-05-26 16:31:40 -0400

    feat: show current url

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ app
   .get("/admin", async (c) => {
     const { pin } = c.cookies;
     if (pin === correctPin) {
-      return await Deno.readTextFile("./admin.html");
+      return (await Deno.readTextFile("./admin.html")).replace("{{site}}", site);
     } else {
       c.setCookie({
         name: "pin",
