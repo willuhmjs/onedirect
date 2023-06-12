2c26c172775e5b5bfb5ddd28c9481b41342f46ae
willuhmjs <willuhmjs@gmail.com>
2023-06-12 17:56:38 -0400

    chore: del log

--- main.ts
+++ main.ts
@@ -59,6 +59,5 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(await site())
 console.log("Server running on http://localhost:8080");
 await app.listen({ port: 8080 });
